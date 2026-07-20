70e369224b5b31819b259562d42844125fc19464
Vinnicius Nascimento <calangowproducer@gmail.com>
2026-07-20 04:21:28 -0300

    feat: create association between Order and OrderItem

--- src/domain/pedido/Pedido.java
+++ src/domain/pedido/Pedido.java
@@ -1,10 +1,13 @@
 package domain.pedido;
 
 import domain.cliente.Cliente;
+import domain.item.ItemPedido;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class Pedido {
     private Cliente cliente;
+    private List<ItemPedido> itens;
     private LocalDateTime dataHora;
 }
