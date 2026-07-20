79275cd67e5a07d7a43f2bb7a737100299300a4a
Vinnicius Nascimento <calangowproducer@gmail.com>
2026-07-20 04:09:47 -0300

    feat: create Order class

---
+++ src/domain/pedido/Pedido.java
@@ -0,0 +1,10 @@
+package domain.pedido;
+
+import domain.cliente.Cliente;
+
+import java.time.LocalDateTime;
+
+public class Pedido {
+    private Cliente cliente;
+    private LocalDateTime dataHora;
+}
