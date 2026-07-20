2b9d43a5c9dc494f4bcc55f26d98c1984801b635
Vinnicius Nascimento <calangowproducer@gmail.com>
2026-07-20 15:00:35 -0300

    feat: create Console class

---
+++ src/util/Console.java
@@ -0,0 +1,16 @@
+package util;
+
+public class Console {
+    {
+        inicioPrograma();
+    }
+
+    public void inicioPrograma(){
+        System.out.println("--------Console--------");
+        System.out.println("1 - Criar Cliente");
+        System.out.println("2 - Criar Produto");
+        System.out.println("3 - Criar Pedido");
+        System.out.println("4 - Listar Pedidos");
+        System.out.println("5 - Sair");
+    }
+}
